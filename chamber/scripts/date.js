db5d890584acbf501562fb98c7ec4bd28f6894ee
KristinLind <kristin.lind@comcast.net>
2025-07-10 18:39:19 -0600

    social

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,3 +1,10 @@
+const menuButton = document.querySelector("#menu-button");
+const navBar = document.querySelector("#nav-bar");
+
+menuButton.addEventListener("click", () => {
+  navBar.classList.toggle("open");
+});
+
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
