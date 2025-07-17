e47e40ad5b4935fc43c7b368fc7b0ac0498d9a9d
KristinLind <kristin.lind@comcast.net>
2025-07-17 14:41:10 -0600

    nav

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuButton = document.getElementById("menu-button");
     const nav = document.querySelector(".navigation");
-
+  
     menuButton.addEventListener("click", () => {
-        nav.classList.toggle("show");
-        menuButton.classList.toggle("open");
+      nav.classList.toggle("show");
+      menuButton.innerHTML = nav.classList.contains("show") ? "&times;" : "&#9776;";
     });
 });
+  
+    
 
