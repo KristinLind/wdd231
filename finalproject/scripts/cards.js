994c96c904a7457f18af021fc92ee9e0c0704188
KristinLind <kristin.lind@comcast.net>
2025-08-06 00:40:53 -0600

    modal

--- finalproject/scripts/cards.js
+++ finalproject/scripts/cards.js
@@ -5,8 +5,5 @@ function adjustLectureCardLayout() {
   window.addEventListener('resize', adjustLectureCardLayout);
   window.addEventListener('DOMContentLoaded', () => {
     adjustLectureCardLayout();
-
-  
-  
-    
+});
   
