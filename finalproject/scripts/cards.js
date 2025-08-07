40821472fc2a11e6f55e76b2da80a05541abc19a
KristinLind <kristin.lind@comcast.net>
2025-08-07 09:59:44 -0600

    base

--- finalproject/scripts/cards.js
+++ finalproject/scripts/cards.js
@@ -1,6 +1,5 @@
 function adjustLectureCardLayout() {
-    console.log("Lecture card layout adjusted on resize or load.");
-  }
+}
   
   window.addEventListener('resize', adjustLectureCardLayout);
   window.addEventListener('DOMContentLoaded', () => {
