7598b4407c17b5f27886d4da137569bfc356cc98
KristinLind <kristin.lind@comcast.net>
2025-08-07 20:05:32 -0600

    html

--- finalproject/scripts/scripts.js
+++ finalproject/scripts/scripts.js
@@ -1,1 +1,14 @@
 localStorage.setItem("lastVisit", new Date().toLocaleString());
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerBtn = document.getElementById("hamburgerBtn");
+    const navMenu = document.querySelector("nav ul");
+  
+    if (hamburgerBtn && navMenu) {
+      hamburgerBtn.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+        hamburgerBtn.classList.toggle("open");
+      });
+    }
+});
+  
